18ef457549f5503d232e2c3908ca42b5d98056ba
scottctaylor12 <>
2017-11-13 14:19:01 -0500

    Comment out line 26

--- SMTP/smtp-client.sh
+++ SMTP/smtp-client.sh
@@ -23,7 +23,7 @@ systemctl start postfix
 sed -i "s/#myhostname = host.domain.tld/myhostname = $HOSTNAME/" /etc/postfix/main.cf
 sed -i "s/#mydomain = domain.tld/mydomain = `echo $HOSTNAME | cut -d "." -f 2,3`/" /etc/postfix/main.cf
 sed -i "s/#myorigin = \$mydomain/myorigin = \$mydomain/" /etc/postfix/main.cf
-sed -i "s/inet_interfaces = localhost/inet_interfaces = loopback-only/" /etc/postfix/main.cf
+sed -i "s/inet_interfaces = localhost/#inet_interfaces = localhost/" /etc/postfix/main.cf
 #sed -i "s/mydestination = \$myhostname, localhost.\$mydomain, localhost/ mydestination = /" /etc/postfix/main.cf
 sed -i "s/#relayhost = \$mydomain/relayhost = $SMTPSERVER/" /etc/postfix/main.cf
 
